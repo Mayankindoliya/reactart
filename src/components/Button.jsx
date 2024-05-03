77651ff8c2e6545429aa250da75ba7515d7ce9e1
Mayank <mayankindoliya@gmail.com>
2024-05-03 16:37:40 +0530

    121. Migrating the Demo App to TailWind CSS.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ children, ...props }) => {
   return (
     <div>
       <button
-        className="px-4 py-2 font-semibold uppercase rounded trext-stone-900 bg-amber-400 hover:bg-amber-500"
+        className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500"
         {...props}
       >
         {children}
